1f5d1049b9dd2f76a9c622ffa735fedc5ed6648f
IG(Ingyum) Kim <59094101+IG-Kim2511@users.noreply.github.com>
2020-11-16 13:42:44 -0500

    Add files via upload

---
+++ precache-manifest.fb47cea0c2a12242f52fa79afeee60b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a115f8373d3075d18a4a69cb449117c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ff2c14bfe9c5f59e92",
+    "url": "/static/css/main.15cc162a.chunk.css"
+  },
+  {
+    "revision": "0cccc350111a0abd1faf",
+    "url": "/static/js/2.3d6296fc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3d6296fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7feacd1c43ed81c2e83e",
+    "url": "/static/js/3.c7986fd8.chunk.js"
+  },
+  {
+    "revision": "e379c741b6c507a38769",
+    "url": "/static/js/4.fb96192f.chunk.js"
+  },
+  {
+    "revision": "89ff2c14bfe9c5f59e92",
+    "url": "/static/js/main.50123e07.chunk.js"
+  },
+  {
+    "revision": "16127beb4216ba779438",
+    "url": "/static/js/runtime-main.ec6372d6.js"
+  }
+]);
